ed5351f8d33d79113b3f7b09e4ea72544da412cf
maxdulay <152431489+maxdulay@users.noreply.github.com>
2024-09-05 20:08:59 -0400

    Added beg (begin) snippet

--- lua/luasnip-latex-snippets/math_i.lua
+++ lua/luasnip-latex-snippets/math_i.lua
@@ -1,8 +1,10 @@
 local M = {}
 
+
 local ls = require("luasnip")
 local utils = require("luasnip-latex-snippets.util.utils")
 local pipe = utils.pipe
+
 
 function M.retrieve(is_math)
   local parse_snippet = ls.extend_decorator.apply(ls.parser.parse_snippet, {
@@ -11,21 +13,26 @@ function M.retrieve(is_math)
     show_condition = is_math,
   }) --[[@as function]]
 
+
   return {
     parse_snippet({ trig = "sum", name = "sum" }, "\\sum_{n=${1:1}}^{${2:\\infty}} ${3:a_n z^n}"),
+
 
     parse_snippet(
       { trig = "taylor", name = "taylor" },
       "\\sum_{${1:k}=${2:0}}^{${3:\\infty}} ${4:c_$1} (x-a)^$1 $0"
     ),
 
+
     parse_snippet({ trig = "lim", name = "limit" }, "\\lim_{${1:n} \\to ${2:\\infty}} "),
     parse_snippet({ trig = "limsup", name = "limsup" }, "\\limsup_{${1:n} \\to ${2:\\infty}} "),
+
 
     parse_snippet(
       { trig = "prod", name = "product" },
       "\\prod_{${1:n=${2:1}}}^{${3:\\infty}} ${4:${TM_SELECTED_TEXT}} $0"
     ),
+
 
     parse_snippet(
       { trig = "part", name = "d/dx" },
@@ -36,7 +43,10 @@ function M.retrieve(is_math)
       "\\frac{\\mathrm{d/${1:V}}}{\\mathrm{d${2:x}}} $0"
     ),
 
+
     parse_snippet({ trig = "pmat", name = "pmat" }, "\\begin{pmatrix} $1 \\end{pmatrix} $0"),
+
+    parse_snippet({ trig = "beg", name = "begin" }, "\\begin{$1}\n $2 \n \\end{$1} $0"),
 
     parse_snippet(
       { trig = "lr", name = "left( right)" },
@@ -63,10 +73,12 @@ function M.retrieve(is_math)
       "\\left< ${1:${TM_SELECTED_TEXT}} \\right>$0"
     ),
 
+
     parse_snippet(
       { trig = "lrb", name = "left\\{ right\\}" },
       "\\left\\{ ${1:${TM_SELECTED_TEXT}} \\right\\\\} $0"
     ),
+
 
     parse_snippet(
       { trig = "sequence", name = "Sequence indexed by n, from m to infinity" },
@@ -74,5 +86,6 @@ function M.retrieve(is_math)
     ),
   }
 end
+
 
 return M
